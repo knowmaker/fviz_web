9dd805913b124d6699e66325b10695a49c30abae
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-08-07 22:14:36 +0300

    new routes.rb

--- backend-rails_api/config/routes.rb
+++ backend-rails_api/config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/create'
-  get 'users/update'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
