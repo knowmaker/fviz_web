cfefc97565f04137212bf8acbd15f81fd012c638
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-08-09 16:08:47 +0300

    trying api interface USER

--- backend-rails_api/spec/requests/users_spec.rb
+++ backend-rails_api/spec/requests/users_spec.rb
@@ -1,70 +1,67 @@
 require 'swagger_helper'
 
 RSpec.describe 'users', type: :request do
-
-  path '/api/signup' do
-
-    post('create user') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
   path '/api/login' do
+    post('login') do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :email, in: :query, type: :string, required: true
+      parameter name: :password, in: :query, type: :string, required: true
 
-    post('login user') do
       response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
         run_test!
       end
     end
   end
 
-  path '/api/profile' do
+  path '/api/users' do
+    post('create user') do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string },
+          last_name: { type: :string },
+          first_name: { type: :string },
+          patronymic: { type: :string }
+        },
+        required: ['email', 'password', 'last_name', 'first_name', 'patronymic']
+      }
 
-    put('show user') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response(201, 'created') do
         run_test!
       end
     end
   end
 
-  path '/api/update' do
+  path '/api/users/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
 
-    post('update user') do
+    get('show user') do
+      tags 'Users'
       response(200, 'successful') do
+        run_test!
+      end
+    end
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+    put('update user') do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string, description: 'id'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string },
+          last_name: { type: :string },
+          first_name: { type: :string },
+          patronymic: { type: :string }
+        }
+      }
+
+      response(200, 'successful') do
         run_test!
       end
     end
