d611d89a685f567af970d13366d6b44451976f57
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-08-23 16:52:49 +0300

    laws spec 1

--- backend-rails_api/spec/requests/laws_spec.rb
+++ backend-rails_api/spec/requests/laws_spec.rb
@@ -1,68 +1,109 @@
 require 'swagger_helper'
 
 RSpec.describe 'laws', type: :request do
+  let(:law_params) do
+    {
+      law_name: 'Sample Law',
+      first_element: 'First Element',
+      second_element: 'Second Element',
+      third_element: 'Third Element',
+      fourth_element: 'Fourth Element',
+      id_type: 123
+    }
+  end
 
   path '/api/laws' do
-
     get('list laws') do
+      tags 'Laws'
       response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
         run_test!
       end
     end
 
     post('create law') do
-      response(200, 'successful') do
+      tags 'Laws'
+      consumes 'application/json'
+      parameter name: :law, in: :body, schema: {
+        type: :object,
+        properties: {
+          law_name: { type: :string },
+          first_element: { type: :string },
+          second_element: { type: :string },
+          third_element: { type: :string },
+          fourth_element: { type: :string },
+          id_type: { type: :integer }
+        },
+        required: ['law_name', 'first_element', 'second_element', 'third_element', 'fourth_element', 'id_type']
+      }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response(201, 'created') do
+        let(:law) { law_params }
+
+        run_test!
+      end
+
+      response(422, 'unprocessable entity') do
         run_test!
       end
     end
   end
 
   path '/api/laws/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show law') do
+      tags 'Laws'
       response(200, 'successful') do
         let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        run_test!
+      end
+
+      response(404, 'not found') do
+        run_test!
+      end
+    end
+
+    put('update law') do
+      tags 'Laws'
+      consumes 'application/json'
+      parameter name: :law, in: :body, schema: {
+        type: :object,
+        properties: {
+          law_name: { type: :string },
+          first_element: { type: :string },
+          second_element: { type: :string },
+          third_element: { type: :string },
+          fourth_element: { type: :string },
+          id_type: { type: :integer }
+        }
+      }
+
+      response(200, 'successful') do
+        let(:id) { '123' }
+        let(:law) { law_params }
+
+        run_test!
+      end
+
+      response(404, 'not found') do
+        run_test!
+      end
+
+      response(422, 'unprocessable entity') do
         run_test!
       end
     end
 
     delete('delete law') do
+      tags 'Laws'
       response(200, 'successful') do
         let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        run_test!
+      end
+
+      response(404, 'not found') do
         run_test!
       end
     end
