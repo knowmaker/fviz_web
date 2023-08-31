b991b40c19000f5590340be16f3da1b74f791785
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-08-31 21:41:55 +0300

    delete user deleting after 3y

--- backend-rails_api/app/mailers/account_reminder_mailer.rb
+++
@@ -1,6 +0,0 @@
-class AccountReminderMailer < ApplicationMailer
-  def reminder_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Просьба войти в систему для сохранения аккаунта')
-  end
-end
