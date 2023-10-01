5fd02e080dcfe7ed99edf3f7b332a8b427b3fe22
knowmaker <59835668+knowmaker@users.noreply.github.com>
2023-10-01 16:27:57 +0300

    debug user register

--- backend-rails_api/app/models/user.rb
+++ backend-rails_api/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :laws, foreign_key: 'id_user'
   has_many :represents, foreign_key: 'id_user'
-  belongs_to :represent, foreign_key: 'active_repr'
+  belongs_to :represent, foreign_key: 'active_repr', optional: true
 
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
@@ -16,4 +16,5 @@ class User < ApplicationRecord
   validates :role, inclusion: { in: [true, false] }
   validates :confirmed, inclusion: { in: [true, false] }
   validates :confirmation_token, presence: true, allow_nil: true
+  validates :active_repr, presence: true, allow_nil: true
 end
